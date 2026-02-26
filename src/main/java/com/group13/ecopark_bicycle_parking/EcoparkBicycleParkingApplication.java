4fc62db37b658e8dcc8b3a91bea6bddc5ff4ae60
Vengeance20 <buicongminh1st27072022@gmail.com>
2026-02-26 17:53:17 +0700

    Khởi tạo project

---
+++ src/main/java/com/group13/ecopark_bicycle_parking/EcoparkBicycleParkingApplication.java
@@ -0,0 +1,13 @@
+package com.group13.ecopark_bicycle_parking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoparkBicycleParkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoparkBicycleParkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group13/ecopark_bicycle_parking/EcoparkBicycleParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group13.ecopark_bicycle_parking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoparkBicycleParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
